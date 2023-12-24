3c1ff2a8df69d511cb58fa1720ca16716dc9fb67
Luc Kusters <ljbkusters@gmail.com>
2023-12-24 09:49:57 +0100

    Add yolo1d package
    Add the yolo1d package which sets up the root structure of the project I
    will be working on. Version and other metadata will go into the
    yolo1d/__metadata.py file.

---
+++ yolo1d/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+__init__.py
+@author Luc Kusters
+@date 24-12-2023
+"""
+from __metadata import VERSION
+
+
+__all__ = ["VERSION"]
---
+++ yolo1d/__metadata.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+__version.py
+@author Luc Kusters
+@date 24-12-2023
+"""
+
+VERSION = "0.1.0"
+
